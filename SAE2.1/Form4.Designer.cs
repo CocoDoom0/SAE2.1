8e65e0955e1992032914d681a8ac0304c8a327ca
Lucas Nogueira <lucas-nogueira@outlook.fr>
2022-05-31 08:59:31 +0200

    test

--- SAE2.1/Form4.Designer.cs
+++ SAE2.1/Form4.Designer.cs
@@ -34,7 +34,7 @@ namespace SAE2._1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 229);
+            this.button1.Location = new System.Drawing.Point(321, 199);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
